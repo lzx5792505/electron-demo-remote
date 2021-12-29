4098ff5cf37c263e89a6ae319dcfc66f5b9ae2b4
liuwenxi <375159680@qq.com>
2021-12-29 11:41:50 +0800

    打包

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+    target: 'electron-main',
+    entry: './main.js',
+    output: {
+        path: path.join(__dirname,'./build'),
+        filename: 'main.js'
+    },
+    mode: 'development',
+    node: {
+        __dirname: false
+    }
+}
